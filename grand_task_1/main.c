e525b5bc404925aeccd2b48c7964953a65f9742a
Ivantea <ivantea2004@gmail.com>
2023-02-12 15:22:59 +0300

    implemented shell sort

--- grand_task_1/main.c
+++ grand_task_1/main.c
@@ -2,13 +2,14 @@
 #include"sort.h"
 #include"generate.h"
 #include"bubble_sort.h"
+#include"shell_sort.h"
 
 int main()
 {
 
 	init_rand();
 
-	test_sort(bubble_sort, gen_descending, less_abs, 10);
+	test_sort(shell_sort, gen_rand, less_abs, 10);
 
 	return 0;
 }
