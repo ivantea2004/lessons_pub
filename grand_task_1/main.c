f80ec9e0948e073ab6616f68b3e957e3c736b214
IvanteaTeam <ivanteateam2004@gmail.com>
2023-02-13 22:51:58 +0300

    added select of tests count

--- grand_task_1/main.c
+++ grand_task_1/main.c
@@ -22,9 +22,9 @@ char* gen_names[GENS_COUNT] = { "ascending", "descending", "random" };
 comparator_t cmps[CMPS_COUNT] = { less, greater,less_abs, greater_abs };
 char* cmp_names[CMPS_COUNT] = { "less", "greater", "less by abs", "greater by abs" };
 
-#define NO_SELECT UINT64_MAX
+#define NO_SELECT INT64_MAX
 
-size_t select_dialog(char** items_names, size_t items_count, char*name)
+size_t select_name_dialog(char** items_names, size_t items_count, char*name)
 {
 	printf("Select %s\n", name);
 	for (size_t i = 0; i < items_count; i++)
@@ -39,30 +39,46 @@ size_t select_dialog(char** items_names, size_t items_count, char*name)
 	return i - 1;
 }
 
+int64_t select_range_dialog(int64_t begin, int64_t end, char *name)
+{
+	printf("Select %s (from %"PRIi64" to %"PRIi64")\n", name, begin, end - 1);
+	int64_t answer;
+	if(scanf("%"SCNi64, &answer) != 1 || !(answer >= begin && answer < end))
+	{
+		printf("Invalid answer\n");
+		return NO_SELECT;
+	}
+	return answer;
+}
+
 int main()
 {
 
 	init_rand();
 	
-	size_t sort1 = select_dialog(sort_names, SORTS_COUNT, "first sort");
+	size_t sort1 = select_name_dialog(sort_names, SORTS_COUNT, "first sort");
 	if (sort1 == NO_SELECT)
 		return 1;
 
-	size_t sort2 = select_dialog(sort_names, SORTS_COUNT, "second sort");
+	size_t sort2 = select_name_dialog(sort_names, SORTS_COUNT, "second sort");
 	if (sort2 == NO_SELECT)
 		return 1;
-	size_t gen = select_dialog(gen_names, GENS_COUNT, "generator");
+	size_t gen = select_name_dialog(gen_names, GENS_COUNT, "generator");
 	if (gen == NO_SELECT)
 		return 1;
-	size_t cmp = select_dialog(cmp_names, CMPS_COUNT, "comparator");
+	size_t cmp = select_name_dialog(cmp_names, CMPS_COUNT, "comparator");
 	if (cmp == NO_SELECT)
+		return 1;
+
+	size_t tests_count = select_range_dialog(1, MAX_TESTS + 1, "number of tests");
+	if(tests_count == NO_SELECT)
 		return 1;
 
 	sort_t run_sorts[2] = { sorts[sort1], sorts[sort2] };
 	char* run_sort_names[2] = { sort_names[sort1], sort_names[sort2] };
-	size_t sizes[] = { 10, 100, 1000, 10000 };
+	size_t sizes[MAX_TESTS] = { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
 
-	run_tests(gens[gen], cmps[cmp], run_sorts, run_sort_names, 2, sizes, 4);
+	run_tests(gens[gen], cmps[cmp], run_sorts, run_sort_names, 2, sizes, tests_count);
 	
 	return 0;
 }
--- grand_task_1/test.h
+++ grand_task_1/test.h
@@ -4,7 +4,7 @@
 
 #include"sort.h"
 
-#define MAX_TESTS 10
+#define MAX_TESTS 7
 
 void run_tests(generator_t gen, comparator_t cmp, sort_t*sorts, char**sort_names, size_t sorts_count, size_t*sizes, size_t sizes_count);
 
